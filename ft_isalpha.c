970c045216795bbfdcb585ed245d1d2965348132
Vineet Meharia <vmeharia@c4a11c4.42berlin.de>
2026-05-16 13:15:44 +0200

    final isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: vmeharia <vmeharia@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 19:30:45 by vmeharia          #+#    #+#             */
-/*   Updated: 2026/05/16 13:09:34 by vmeharia         ###   ########.fr       */
+/*   Updated: 2026/05/16 13:15:19 by vmeharia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,10 @@ int	ft_isalpha(int c)
 }
 /*
 #include <stdio.h>
-#include <strings.h>
+#include <ctype.h>
 
 int	main(void)
 {
 	printf("%d\n", ft_isalpha('A'));
 	printf(%d\n", isalpha('A'));
-	return (0);
 */
