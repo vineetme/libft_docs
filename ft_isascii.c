b23667b5bceabb36e7d5031fcb37818c0491d1ab
Vineet Meharia <vmeharia@c4a11c4.42berlin.de>
2026-05-16 13:11:06 +0200

    final isascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: vmeharia <vmeharia@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 19:29:13 by vmeharia          #+#    #+#             */
-/*   Updated: 2026/05/11 19:29:25 by vmeharia         ###   ########.fr       */
+/*   Updated: 2026/05/16 13:07:20 by vmeharia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,13 @@ int	ft_isascii(int c)
 	else
 		return (0);
 }
+/*
+#include <stdio.h>
+#include <ctype.h>
+
+int	main(void)
+{
+	printf("%d\n", ft_isascii(45));
+	printf("%d\n", isascii(45));
+}
+*/
